a36b10769736b82d3cff2ad7bbf996e708f51ca5
nguyen thanh tung <thanhtung776@gmail.com>
2017-06-09 10:46:36 +0700

    youtube search api

--- reactYourtube/src/index.js
+++ reactYourtube/src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDom from 'react-dom';
+import YoutubeApiSearch from 'youtube-api-search';
+
 import SearchBar from './components/search_bar'; //SearchBar bắt buộc viết hoa ký tự đầu tiên "S"
 const API_KEY = 'AIzaSyB4sG5i9jS3PC1pa4pj4ZjgG8XmjM8NVzE';
+
+YoutubeApiSearch({key: API_KEY, term: 'surfboards'}, function(data){
+    console.log(data);
+})
+
 //create new compoment
 const App = ()=>{
     return (
