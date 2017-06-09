7ad1522f1ab84e1d8b2657ff7300ad5b57fb4b62
nguyen thanh tung <thanhtung776@gmail.com>
2017-06-09 14:36:34 +0700

    list video

--- reactYourtube/src/components/video_list.js
+++ reactYourtube/src/components/video_list.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import VideoListItem from './video_list_item';
 
 const VideoList = (props)=>{
-    const videoItems =  props.videos.map(function(){
-        return <VideoListItem />;
+    const videoItems =  props.videos.map(function(video){
+        return <VideoListItem key = {video.etag} video = { video } />;
     });
     return (
         <ul className = "col-md-4 list-group">
