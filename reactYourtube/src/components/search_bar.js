92217aeeed034341e934d536dde98f2653d34407
nguyen thanh tung <thanhtung776@gmail.com>
2017-06-09 10:36:29 +0700

    component state

--- reactYourtube/src/components/search_bar.js
+++ reactYourtube/src/components/search_bar.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class SearchBar extends React.Component {
-    render() {
-        return <input />;
+class SearchBar extends Component {
+    constructor(props){
+        super(props);
+        this.state = { term: ''}        
     }
+    render() {
+        return (
+        <div>
+            <input onChange = {event =>this.setState({term: event.target.value}) } />
+        </div>    
+        );
+    }
+
 }
 
 export default SearchBar;
