158c67210d3823f041e312c1f037af1dbabca58e
nguyen thanh tung <thanhtung776@gmail.com>
2017-06-09 10:59:26 +0700

    refactor App to class

--- reactYourtube/src/index.js
+++ reactYourtube/src/index.js
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDom from 'react-dom';
 import YoutubeApiSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar'; //SearchBar bắt buộc viết hoa ký tự đầu tiên "S"
 const API_KEY = 'AIzaSyB4sG5i9jS3PC1pa4pj4ZjgG8XmjM8NVzE';
 
-YoutubeApiSearch({key: API_KEY, term: 'surfboards'}, function(data){
-    console.log(data);
-})
-
 //create new compoment
-const App = ()=>{
-    return (
-    <div>
-        <SearchBar/>
-    </div>
-    );
-}
+class App extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            videos: []
+        }
+
+        YoutubeApiSearch({key: API_KEY, term: 'surfboards'}, (videos)=>{
+            this.setState({
+                videos
+            });
+        });
+    }
+
+    render(){
+        return (
+            <div>
+                <SearchBar/>
+            </div>
+        );
+    }
+} 
 
 ReactDom.render(<App />, document.querySelector(".container"));
