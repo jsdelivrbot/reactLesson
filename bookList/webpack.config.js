3ffd54889f0a8ec307e7c3c384c2b75e2d8920ff
nguyen thanh tung <thanhtung776@gmail.com>
2017-06-10 07:42:42 +0700

    re-create template for react redux

--- bookList/webpack.config.js
+++ bookList/webpack.config.js
@@ -1,26 +1,8 @@
-module.exports = {
-  entry: [
-    './src/index.js'
-  ],
-  output: {
-    path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
-      }
-    }]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './'
-  }
-};
+switch (process.env.APP_ENV) {
+    case 'production':
+        module.exports = require('./webpack/webpack.production');
+        break;
+    default:
+        module.exports = require('./webpack/webpack.development');
+        break;
+}
