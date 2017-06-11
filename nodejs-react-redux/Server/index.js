7ae2590ed7ebb335a3552a0b67dc8c36732f004b
tung776 <thanhtung776@gmail.com>
2017-06-11 22:35:45 +0700

    config webpack support for react

--- nodejs-react-redux/Server/index.js
+++ nodejs-react-redux/Server/index.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import path from 'path';
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware'
+import webpackConfig from '../webpack.config.dev';
 let app = express();
 
-app.get('/*', (req, res, next)=>{
+app.use(webpackMiddleware(webpack(webpackConfig)));
+app.get('/', (req, res, next)=>{
     res.sendFile(path.join(__dirname, './index.html'));
 });
 
