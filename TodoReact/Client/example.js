e1d28da13504d73310e139c15d6ee42d6b05fa1c
nguyen thanh tung <thanhtung776@gmail.com>
2017-06-14 09:34:29 +0700

    dispatch ADD_NEW_NOTE and REMOVE_NOTE

--- TodoReact/Client/example.js
+++ TodoReact/Client/example.js
@@ -9,6 +9,12 @@ const reducer = (state = defaultState, action)=>{
     switch(action.type){
         case 'TOGGLE_IS_ADDING_NOTE':
             return {...state, isAdding: !state.isAdding}
+        case 'ADD_NEW_NOTE':
+            return {...state, mang: [...state.mang, action.newNote]}
+        case 'REMOVE_NOTE':
+            return {...state, mang:[...state.mang.slice((value, key)=>{
+                return key != action.IndexRemoveNote;
+            })]}
         default:
             return state;
     }
@@ -19,3 +25,16 @@ const store = createStore(reducer);
 console.log('before dispatch ',store.getState());
 store.dispatch({type: 'TOGGLE_IS_ADDING_NOTE'})
 console.log('after dispatch ',store.getState());
+
+store.dispatch({
+    type: 'ADD_NEW_NOTE',
+    newNote: 'GraphQL'
+})
+
+console.log('after dispatch ADD_NEW_NOTE ',store.getState());
+
+store.dispatch({
+    type: 'REMOVE_NOTE',
+    IndexRemoveNote: '4'
+});
+console.log('after dispatch REMOVE_NOTE ',store.getState());
