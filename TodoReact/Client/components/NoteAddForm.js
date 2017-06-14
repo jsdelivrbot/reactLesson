3093d121ff370b323c1cfcd2498761ceabc74ae1
nguyen thanh tung <thanhtung776@gmail.com>
2017-06-14 08:00:17 +0700

    show/hide form

--- TodoReact/Client/components/NoteAddForm.js
+++ TodoReact/Client/components/NoteAddForm.js
@@ -2,30 +2,48 @@ import React from 'react';
 class NoteAddForm extends React.Component {
     constructor(props){
         super(props);
-        this.state = { note : ""};
+        this.state = { 
+            note : "",
+            isAdding: false
+        };
         this.onNoteChange = this.onNoteChange.bind(this);
-        this.onAddNote = this.onAddNote.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+        this.onAdd = this.onAdd.bind(this);
     }
     onNoteChange(e){
         this.setState({note: e.target.value});
     }
-    onAddNote(e){
+    onSubmit(e){
         e.preventDefault();
         this.props.handleAddNote(this.state.note);
-        this.setState({note: ''});
+        this.setState({
+            note: '',
+            isAdding: !this.state.isAdding
+        });
+    }
+    onAdd(){
+        this.setState({isAdding: !this.state.isAdding});
     }
     render(){
-        return(
-            <div>
-               <h1>Add</h1>
-               <form onSubmit = {this.onAddNote}  action="">
-                    <div className="input-group">
-                        <input value = {this.state.note} onChange = {this.onNoteChange} type="text"/>
-                        <button className="btn btn-primary">Add</button>
-                    </div>
-               </form>
-            </div>
-        );
+        if(this.state.isAdding){
+            return(
+                <div>
+                <form onSubmit = {this.onSubmit}  action="">
+                        <div className="input-group">
+                            <input value = {this.state.note} onChange = {this.onNoteChange} type="text"/>
+                            <button className="btn btn-primary">Add</button>
+                        </div>
+                </form>
+                </div>
+            );
+        }
+        else {
+            return(
+                <div>
+                    <button onClick = {this.onAdd} className="btn btn-success">+</button>
+                </div>
+            );
+        }
     }
 }
 export default NoteAddForm;
