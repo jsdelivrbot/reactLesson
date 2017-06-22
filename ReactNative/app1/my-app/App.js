a9c4eb266dc4a0ef1c14bbd8494097290ea8f488
nguyen thanh tung <thanhtung776@gmail.com>
2017-06-22 10:15:42 +0700

    ..

--- ReactNative/app1/my-app/App.js
+++ ReactNative/app1/my-app/App.js
@@ -5,8 +5,9 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
+        <Text>Hello world!</Text>
+        <Text>This is the firt react native application</Text>
+        <Text>Let go! to build some android applications</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
