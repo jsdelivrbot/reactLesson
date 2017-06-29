2a794d778760373495f809ca771878abec04d5cc
tung776 <thanhtung776@gmail.com>
2017-06-29 22:16:38 +0700

    video 45 styling

--- ReactNative/albums/src/components/albumDetail.js
+++ ReactNative/albums/src/components/albumDetail.js
@@ -1,14 +1,36 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import Card from './card';
 import CardSection from './cardSection';
 
-const AlbumDetail = (props) => (
-        <Card>
-            <CardSection>
-                <Text>{props.album.title}</Text>
-            </CardSection>
-        </Card>
-    );
+const AlbumDetail = (props) => {
+
+    const { title, artist, thumbnail_image } = props.album;
+    const { headerContentStyle, thumbnailStyle } = styles;
+        return (
+            <Card>
+                <CardSection>
+                    <View>
+                        <Image style = {thumbnailStyle} source = {{ uri: thumbnail_image }} />
+                    </View>
+                    <View style = { headerContentStyle }>
+                        <Text>{ title }</Text>
+                        <Text>{ artist }</Text>
+                    </View>
+                </CardSection>
+            </Card>
+        );
+};
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    }
+}
 
 export default AlbumDetail;
